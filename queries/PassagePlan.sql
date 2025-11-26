c5ba033108f659e0bae0bf7913d688737f51fa90
Dimitri Skliros <skliros@quantsuite.io>
2025-11-26 12:35:23 +0000

    Updated SQL query to conform to production criteria

--- queries/PassagePlan.sql
+++ queries/PassagePlan.sql
@@ -6,7 +6,6 @@ es.id=3
 schedule_frequency_hours=0.5
 lookback_days=1
 */
-
 SELECT
 	et.id AS event_type_id,
 	et.name AS event_type_name,
@@ -22,18 +21,15 @@ SELECT
 FROM
 	events e
 LEFT JOIN vessels v ON v.id = e.vessel_id
---LEFT JOIN vessel_subtypes vs ON vs.id = v.subtype_id
 LEFT JOIN event_details ed ON ed.event_id = e.id
 LEFT JOIN event_statuses es ON es.id = ed.status_id
 LEFT JOIN event_types et ON et.id = e.type_id
 WHERE
 	et.id = 37  --passage-plan
-	--AND es.id = 3   --for-review
-	--AND LOWER(e.name) NOT LIKE '%test%'
-	--AND LOWER(e.name) NOT LIKE '%vessel%'
+	AND es.id = 3   --for-review
+	AND LOWER(e.name) NOT LIKE '%test%'
+	AND LOWER(e.name) NOT LIKE '%vessel%'
 	AND ed.synced_at >= NOW() - INTERVAL '1 day' * :lookback_days
 	AND e.deleted_at IS NULL
-	--AND v.email IS NOT NULL
-	--AND v.name IS NOT NULL
 ORDER BY
 	ed.synced_at ASC;
