cfd735683f259adef8ebf61bef7653d44c4a1d87
Dimitri Skliros <skliros@quantsuite.io>
2025-12-05 14:38:51 +0200

    New query, replaces event status wording

--- queries/PassagePlan.sql
+++ queries/PassagePlan.sql
@@ -1,32 +1,32 @@
 /*
 event_type='Passage Plan'
 et.id=37
-es.name='for-review'
-es.id=3
+es.name='PASSAGE PLAN - FOR OFFICE NOTES'   <-- NEW
+es.id=9                                     <-- NEW
 schedule_frequency_hours=0.5
 lookback_days=1
 */
 SELECT
-	et.id AS event_type_id,
-	et.name AS event_type_name,
-	v.email AS vsl_email,
-	v.id AS vessel_id,
-	v.name AS vessel_name,
-	e.id AS event_id,
-	e.name AS event_name,
-	e.created_at,
-	ed.synced_at,
-	es.name AS status,
-	es.id AS status_id
+    et.id AS event_type_id,
+    et.name AS event_type_name,
+    v.email AS vsl_email,
+    v.id AS vessel_id,
+    v.name AS vessel_name,
+    e.id AS event_id,
+    e.name AS event_name,
+    e.created_at,
+    ed.synced_at,
+    es.name AS status,
+    es.id AS status_id
 FROM
-	events e
+    events e
 LEFT JOIN vessels v ON v.id = e.vessel_id
 LEFT JOIN event_details ed ON ed.event_id = e.id
 LEFT JOIN event_statuses es ON es.id = ed.status_id
 LEFT JOIN event_types et ON et.id = e.type_id
 WHERE
-	et.id = 37  --passage-plan
-	AND es.id = 3   --for-review
+    et.id = 37  --passage-plan
+	AND es.id = 9  --passage-plan-for-office-notes  <-- NEW
 	AND LOWER(e.name) NOT LIKE '%test%'
 	AND LOWER(e.name) NOT LIKE '%vessel%'
 	AND ed.synced_at >= NOW() - INTERVAL '1 day' * :lookback_days
